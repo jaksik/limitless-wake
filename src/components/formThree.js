e79f118d810bb37daec8cb23f936217c625d6757
connor <connor.jaksik@gmail.com>
2019-03-29 14:30:00 -0600

    added lake selection to contact form

--- src/components/formThree.js
+++ src/components/formThree.js
@@ -14,17 +14,20 @@ class Form extends Component {
         <input name="name" placeholder="Your First and Last Name" type="text" /><br />
         <input name="email" placeholder="Your Email" type="email" /><br />
         <input name="phone" placeholder="Your Phone Number" type="number" /><br />
-
         <label for="meeting-time">Preffered lesson day and time:</label>
-
+        <select name="preffered-lake">
+          <option> </option>
+          <option value="Austin">Lake Austin</option>
+          <option value="Travis">Lake Travis</option>
+          <option value="LBJ">Lake LBJ</option>
+          <option value="Canyon">Canyon Lake</option>
+        </select>
         <input type="datetime-local"
           id="meeting-time"
           name="meeting-time" value=""
           min="2019-01-03T00:00"
           max="2020-31-31T00:00" />
-
-        
-
+          <br/>
         <input name="message" placeholder="Your Message" type="textarea" /><br />
         <button>Submit</button>
       </form>
