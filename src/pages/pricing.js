df6cbb162bb0d46b91890382620023209ee15250
connor <connor.jaksik@gmail.com>
2019-05-02 16:21:38 -0600

    updated pricing page

--- src/pages/pricing.js
+++ src/pages/pricing.js
@@ -7,17 +7,22 @@ const IndexPage = () => (
   <Layout>
     <SEO title="Pricing" keywords={[`wake`, `lessons`, `price`, `austin`, `texas`]} />
     <h1>Lesson Pricing</h1>
-    
-      <strong>Lessons Start at $100/hour</strong><br/>
-      <strong>Lessons + Gear Start At $175/hour</strong>
-      <br/>
-      <br/>
-      <p>*An hour booked is an hour in the water.</p>
-      <p>*We accept paypal, venmo, and cash at the day of your lesson.</p>
-      <p>*Hours can be split between up to 3 riders.<br/>For example an hour split between 3 riders will give each rider 20 minutes "in the water" lesson time.</p>
-      <p>*Lessons purchased with gear will include wakeboard/wakesurf, rope, and a jacket(1).</p>
-  
-    <Button/>
+    <strong>Lessons Start at $100/hour</strong><br />
+    <strong>Lessons + Gear Start At $175/hour</strong>
+    <br/>
+      <p style={{ marginTop: `20px`}}>3 riders? We suggest 1-2 hours<br/>
+      4 riders? We suggest 2-3 hours<br/>
+      4+ riders?” We suggest 3+ hours</p>
+    <br />
+    <br />
+    <div style={{ textAlign: `left`, maxWidth: `960px`, padding: `20px`, margin: `0 auto`}}>
+    <p>*An hour booked is an hour in the water.</p>
+    <p>*We accept paypal, venmo, and cash at the day of your lesson.</p>
+    <p>*Hours can be sppt between up to 3 riders. For example an hour sppt between 3 riders will give each rider 20 minutes "in the water" lesson time.</p>
+    <p>*Lessons purchased with gear will include wakeboard/wakesurf, rope, and a jacket (1).</p>
+
+    </div>
+    <Button />
   </Layout>
 )
 
