35b72c2bfc879e29ef23f043f4845a94a5b9afac
connor <connor.jaksik@gmail.com>
2019-06-07 05:23:45 -0600

    testing complex form, no other page content

--- src/pages/contact/index.js
+++ src/pages/contact/index.js
@@ -8,17 +8,8 @@ import waiver from "../../data/waiver.pdf"
 
 const Contact = () => (
   <Layout>
-    <div
-      style={{
-        margin: `0 auto`,
-        maxWidth: `960px`,
-        paddingTop: 0,
-      }}
-    >
-      <SEO title="Contact" keywords={[`wakeboard`, `lessons`, `austin`, `texas`, `limitless`, `wake`, `lake`, `travis`, `surf`]} />
-      <h1 style={{ margin: `30px 0`, textAlign: `center` }}>Book A Lesson</h1>
-
-      <form
+    
+    <form
                 name="contact"
                 method="post"
                 data-netlify="true"
@@ -69,18 +60,7 @@ const Contact = () => (
                         </select>
                
                     <button style={{ margin: `15px auto`, borderRadius: `10px` }}>Submit</button>
-            </form>     
-      <br />
-      <a href={waiver} download>
-        <Button color="primary" size="lg" style={{ width: `55%`, margin: `5px auto`, zIndex: `1` }} block id="cover-button">Download The Waiver</Button>
-      </a>
-      <div style={{ textAlign: `center` }}>
-        <h5>Have Questions?</h5>
-        <p>Always feel free to give us a call or send us an email.</p>
-        <p><strong>Phone: </strong> 512-270-1735</p>
-        <p><strong>Email:</strong> limitlesswaketech@gmail.com</p>
-      </div>
-    </div>
+            </form>  
   </Layout>
 )
 
